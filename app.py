5547acfe5b40c8d9201d28dd1a1d05c8c762ee18
anonimappadmin <gilles.dev30@gmail.com>
2025-04-09 11:41:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -556,7 +556,7 @@ def inject_now():
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', debug=True, port=int(os.environ.get('PORT', 5000)))
 
 print(app.url_map)
 
